5c3e34e5d57a8ac7f6b20bd8eb2cacaa03b6d4dd
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2022-10-10 16:32:43 +0200

    Initial Commit

---
+++ Guess the flag/Guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_flagApp.swift
+//  Guess the flag
+//
+//  Created by Codemaker on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
