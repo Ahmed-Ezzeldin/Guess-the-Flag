7ffb897d18a648f07dab17722fe25659fcf2e60b
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2022-10-10 17:10:56 +0200

    add main features

--- Guess the flag/ContentView.swift
+++ Guess the flag/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+
+    @State private  var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.blue
+                .opacity(0.8)
+                .ignoresSafeArea()
+            VStack (spacing: 30){
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
+        }
+        .alert(scoreTitle, isPresented: $showingScore){
+            Button ("Continue" , action: askQuestion)
+        } message: {
+            Text("Your score is 000")
+        }
+    }
+    func flagTapped (_ number: Int){
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+
+    func askQuestion() {
+        countries = countries.shuffled()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
